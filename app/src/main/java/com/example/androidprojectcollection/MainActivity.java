94d0be3b8e48926e64888d6ccf9c66fcf2f3b7cf
Fiel <fiellouisomasas@gmail.com>
2024-02-28 15:00:00 +0800

    add calculator progress

--- app/src/main/java/com/example/androidprojectcollection/MainActivity.java
+++ app/src/main/java/com/example/androidprojectcollection/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1, btn2;
+    Button btn1, btn2, btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent2 = new Intent(MainActivity.this, ButtonExercise.class);
                 startActivity(intent2);
+            }
+        });
+
+        btn3 = (Button) findViewById(R.id.btnCalculatorExercise);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(MainActivity.this, CalculatorExercise.class);
+                startActivity(intent3);
             }
         });
     }
